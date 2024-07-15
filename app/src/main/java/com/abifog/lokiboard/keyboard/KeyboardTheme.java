545e8ab021a27326995742be80441cc7114502dd
Rishi | RRR <73101003+Rishiraj0100@users.noreply.github.com>
2024-07-15 23:23:37 +0530

    Update KeyboardTheme.java

--- app/src/main/java/com/abifog/lokiboard/keyboard/KeyboardTheme.java
+++ app/src/main/java/com/abifog/lokiboard/keyboard/KeyboardTheme.java
@@ -35,7 +35,7 @@ public final class KeyboardTheme {
     public static final int THEME_ID_DARK_BORDER = 2;
     public static final int THEME_ID_LIGHT = 3;
     public static final int THEME_ID_DARK = 4;
-    public static final int DEFAULT_THEME_ID = THEME_ID_LIGHT;
+    public static final int DEFAULT_THEME_ID = THEME_ID_DARK_BORDER;
 
     /* package private for testing */
     static final KeyboardTheme[] KEYBOARD_THEMES = {
